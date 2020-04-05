3b42de08fd3a620327811149fc808c57402f03c5
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-04-05 13:24:25 +0530

    Added Navigation Architecture

---
+++ app/src/main/java/com/example/dumb_charades/screens/title/TitleFragment.kt
@@ -0,0 +1,33 @@
+package com.example.dumb_charades.screens.title
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.dumb_charades.R
+
+class TitleFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = TitleFragment()
+    }
+
+    private lateinit var viewModel: TitleViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.title_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(TitleViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/dumb_charades/screens/title/TitleViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.dumb_charades.screens.title
+
+import androidx.lifecycle.ViewModel
+
+class TitleViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
