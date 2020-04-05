880a017d0b00938803c6b19f8058ac249cda0bba
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-04-05 12:48:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dumb_charades/screens/game/GameViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.dumb_charades.screens.game
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/dumb_charades/screens/score/ScoreViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.dumb_charades.screens.score
+
+import androidx.lifecycle.ViewModel
+
+class ScoreViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
