22412ef5a81d14f7d9ede25bd79b8f16e243c7b8
Sugandh Kumar <sugandhpatna95@gmail.com>
2020-04-27 21:15:34 +0530

    Implemented Motion Layout

--- app/src/main/java/com/example/dumb_charades/MovieFragment.kt
+++ app/src/main/java/com/example/dumb_charades/MovieFragment.kt
@@ -1,33 +1,44 @@
 package com.example.dumb_charades
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
-
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.dumb_charades.databinding.MovieDestinationBinding
 
 class MovieFragment : Fragment() {
 
+    private lateinit var binding : MovieDestinationBinding
     private  var isLargeLayout = false
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = DataBindingUtil.inflate(inflater,R.layout.movie_destination, container,false)
 
         val constraintSet1 = ConstraintSet()
         val constraintSet2 = ConstraintSet()
 
-        constraintSet2.clone()
-
+        constraintSet2.clone(context,R.layout.movie_destination_large)
+        val constraintLayout = binding.constraintLayout
+        constraintSet1.clone(constraintLayout)
+        val posterImageView = binding.imageViewPoster
+        posterImageView.setOnClickListener {
+            TransitionManager.beginDelayedTransition(constraintLayout)
+            if (isLargeLayout) {
+                constraintSet1.applyTo(constraintLayout)
+            } else {
+                constraintSet2.applyTo(constraintLayout)
+            }
+            isLargeLayout = !isLargeLayout
+        }
+        return binding.root
     }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.movie_destination, container, false)
-    }
-
 }
